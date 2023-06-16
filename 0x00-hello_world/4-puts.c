fefa16c7efec0ed921f6cc59363397019c99447b
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-15 17:55:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * * main - This program prints a line with puts function
  *   Return: 0 (successful)
  */
-in main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
