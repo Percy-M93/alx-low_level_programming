3ee9b413fa8a228148ce298ef7b1dff930b6a6dc
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-15 17:41:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <std.h>
+/**
+ * main - A program that prints a line with push function
+ *  Return: 0 (seccessful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
