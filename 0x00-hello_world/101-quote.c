dc5030a8af980539449bca110cd5e273d99733cf
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-15 19:09:20 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This program prints a line without using the printf or puts function
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
+return (1);
+}
+
