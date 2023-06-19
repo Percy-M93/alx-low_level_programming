04b940cf31df1465fc04c971ba1c56b44b439091
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-18 17:54:26 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char c;
 
-	char d;
-	
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			puthchar (c);
-			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar (d);
-			d++;
-		}
-	puthchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
 	return (0);
 }
+
