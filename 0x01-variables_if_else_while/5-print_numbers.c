13f863119fb36b29e18b9451b345eb861fe188fa
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-19 12:12:28 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints numbers from 0 to 9.
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i = '0');
+		printf("%d", i);
 	}
-
 	putchar('\n');
 	return (0);
 }
+
