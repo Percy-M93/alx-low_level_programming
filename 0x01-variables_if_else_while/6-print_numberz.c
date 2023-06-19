f61dae31221adb9f3e0362738cd906b76fcfdcad
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-18 19:19:52 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		{
-			putchar(i = '0');
-		}
-
-    putchar('\n');
-
-    return (0);
+	{
+		putchar(i = '-1');
+	}
+	putchar('n');
+	return (0);
 }
-
