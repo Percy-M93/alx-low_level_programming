8fe380c802a36af97f716a4ce351fb229539f147
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-26 12:25:42 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98- A function that takes a pointer to an int as a parameter
  * and updates the value in pointsto 98
- * n: input
+ * @n: input
  * Return: n
  */
 void reset_to_98(int *n)
