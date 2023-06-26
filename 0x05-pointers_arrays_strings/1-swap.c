4cb0ace708b8006dca415592a9ca5b76ea68b511
Percy Mdluli <Percy.Mdluli@icloud.com>
2023-06-26 12:33:03 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: integer to be swapped
+ * @b: integer to be swapped
+ */
+void swap_int(in *a, int *b)
+{
+	int m;
+	
+	m = *a;
+	*a = *b;
+	*b = m;
+}
